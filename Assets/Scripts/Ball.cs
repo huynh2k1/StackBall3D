9bc4030421b7a2d85224605152525c1f1564099d
huynh2k1 <93506417+huynh2k1@users.noreply.github.com>
2023-09-09 17:49:40 +0700

    gameplay

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+    public bool smash;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            smash = true;
+        if (Input.GetMouseButtonUp(0))
+            smash = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0) && smash == true)
+        {
+            //smash = true;
+            rb.velocity = new Vector3(0, -100 * Time.fixedDeltaTime * 7, 0);
+        }
+
+        if (rb.velocity.y > 5)
+            rb.velocity = new Vector3(rb.velocity.x, 5, rb.velocity.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!smash)
+        {
+            rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+        }
+        else
+        {
+            if(collision.gameObject.tag == "enemy")
+            {
+                Destroy(collision.transform.parent.gameObject);
+            }
+            if(collision.gameObject.tag == "plane")
+            {
+                Debug.Log("Over");
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnCollisionStay(Collision target)
+    {
+        if(!smash || target.gameObject.tag == "Finish")
+        {
+            rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+            smash = false;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 camFollow;
+    public Transform ball;
+    private Transform Win;
+
+    private void Awake()
+    {
+        
+    }
+    private void Update()
+    {
+        if(Win == null)
+        {
+            Win = GameObject.Find("Win(Clone)").GetComponent<Transform>();
+        }
+        if(ball == null)
+        {
+            return;
+        }    
+        //Nếu vị trí y của cam > vị trí y của ball AND vị trí y của cam > vị trí y của win + 4
+        if(transform.position.y > ball.transform.position.y && transform.position.y > Win.position.y + 4f)
+        {
+            camFollow = new Vector3(transform.position.x, ball.position.y, transform.position.z);
+        }
+        transform.position = new Vector3(transform.position.x, camFollow.y, -5);
+    }
+}
